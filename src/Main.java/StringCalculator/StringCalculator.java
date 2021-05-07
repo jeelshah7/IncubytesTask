45accc23ac643fcf6e015101205dbe84915e085a
Jeel Shah <shahjeel.oo7@gmail.com>
2021-05-07 12:23:51 +0530

    Task 4 support of delimeter added.

--- src/Main.java/StringCalculator/StringCalculator.java
+++ src/Main.java/StringCalculator/StringCalculator.java
@@ -8,23 +8,36 @@ public class StringCalculator {
         if (numbers.length()==0){
             return 0;
         }
-
+        String delimiter = "";
         int sum = 0;
-        String[] numberListWithCommas = numbers.split("\n");
         
+        if(numbers.startsWith("//")) {
+        	String delimeter = numbers.substring(2);
+        	delimeter = delimeter.substring(0, delimeter.indexOf("\n"));
+        	numbers = numbers.substring(numbers.indexOf("\n")+1,numbers.length());
+        	sum = addWithDelimeter(numbers,delimeter);
+   
+        }else {
+        	sum = addWithDelimeter(numbers,",");
+        }
+        
+        return sum;
+    }
+    
+    public int addWithDelimeter(String numbers,String delimeter) {
+    	int sum = 0;
+    	String[] numberListWithDelimeter = numbers.split("\n");
+    	
         ArrayList<String> numberList = new ArrayList<String>();
-        
-        for(String num: numberListWithCommas) {
-        	String[] numbersWithoutCommas = num.split(",");
-        	List<String> stringList = new ArrayList<String>(Arrays.asList(numbersWithoutCommas));
+        for(String num: numberListWithDelimeter) {
+        	String[] numbersWithoutDelimeter = num.split(delimeter);
+        	List<String> stringList = new ArrayList<String>(Arrays.asList(numbersWithoutDelimeter));
         	numberList.addAll(stringList);
         }
         
         for(String num: numberList) {
         	sum += Integer.parseInt(num);
-        	
         }
-        
         return sum;
     }
 }
--- src/Main.java/Tests/StringCalculatorTest.java
+++ src/Main.java/Tests/StringCalculatorTest.java
@@ -48,4 +48,12 @@ public class StringCalculatorTest {
 
         assertEquals(6,ans);
     }
+
+    @Test
+    public void testDelimeter() {
+    	StringCalculator calculator = new StringCalculator();
+        int ans = calculator.Add("//;\n1;2");
+        
+        assertEquals(3,ans);
+    }
 }
